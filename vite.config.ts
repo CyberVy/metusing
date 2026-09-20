ffda365d2d6b924818af6d9688ecd33d3e194ce1
CyberVy <923353275@qq.com>
2026-09-20 17:02:31 +0800

    dev: use strict port in vite

--- vite.config.ts
+++ vite.config.ts
@@ -114,6 +114,7 @@ export default defineConfig({
     },
     server: {
         port: 4001,
+        strictPort: true,
         host: "0.0.0.0",
         fs: {
             allow: [path.resolve(import.meta.dirname)]
